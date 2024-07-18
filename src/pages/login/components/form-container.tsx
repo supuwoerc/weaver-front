d6d90bc1c78f120bb0c57856913474a7d81156c6
supuwoerc <zhangzhouou@gmail.com>
2024-07-18 22:24:50 +0800

    fix:  修复暂时死区&样式问题

--- src/pages/login/components/form-container.tsx
+++ src/pages/login/components/form-container.tsx
@@ -2,11 +2,11 @@ import styled from "@emotion/styled"
 
 const FormContainer = styled.div`
     .title {
-        color: #1d2230;
+        color: var(--gray-10);
         font-size: 24px;
     }
     .desc {
-        color: #88909c;
+        color: var(--gray-8);
         font-size: 16px;
         margin: 4px 0 20px;
     }
