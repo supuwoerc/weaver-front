aff8c6592e4b934578ce1cf238c0471e76c166ef
supuwoerc <zhangzhouou@gmail.com>
2025-01-10 18:02:35 +0800

    chore: scan

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
+import { scan } from "react-scan"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
+import { appIsDevEnv } from "./constant/system.ts"
+scan({
+    enabled: appIsDevEnv,
+    playSound: appIsDevEnv,
+    showToolbar: true,
+})
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />)
