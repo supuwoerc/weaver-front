e9332460bd3dfccbe181d66c5856029cf814714d
supuwoerc <zhangzhouou@gmail.com>
2025-08-31 11:16:06 +0800

    feat: locale

--- src/lib/intl/en/permission.ts
+++ src/lib/intl/en/permission.ts
@@ -18,10 +18,10 @@ const enUS: SystemLocaleMapping = {
     "permission.modal.label.roles": "Roles",
     "permission.modal.placeholer.roles": "Please configure permission roles",
     "permission.modal.rule.required": "Required",
-    "resource.type.view_route": "Frontend route",
-    "resource.type.view_menu": "Frontend menu",
-    "resource.type.view_resource": "Frontend resource",
-    "resource.type.api_route": "Backend Api",
+    "resource.type.view_route": "frontend route",
+    "resource.type.view_menu": "frontend menu",
+    "resource.type.view_resource": "frontend resource",
+    "resource.type.api_route": "backend api",
 }
 
 export default enUS
