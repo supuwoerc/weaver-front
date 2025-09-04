6054d0486544d13f290ed64e51fe5850cecff057
supuwoerc <zhangzhouou@gmail.com>
2025-09-04 21:43:36 +0800

    chore: remove var

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,11 +4,6 @@ interface ImportMetaEnv {
     readonly VITE_APP_NAME: string
     readonly VITE_APP_TITLE: string
     readonly VITE_APP_ENV: string
-    readonly VITE_APP_TOKEN_KEY: StorageState["token"]
-    readonly VITE_APP_REFRESH_TOKEN_KEY: StorageState["refreshToken"]
-    readonly VITE_APP_LOCALE_KEY: StorageState["language"]
-    readonly VITE_APP_COLLAPSE_KEY: StorageState["collapse"]
-    readonly VITE_APP_THEME_KEY: StorageState["theme"]
     readonly VITE_APP_DEFAULT_SERVER: string
 }
 
