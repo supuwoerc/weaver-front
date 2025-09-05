f79712b895496a6630897dd67a8aed379e3ec8d3
supuwoerc <zhangzhouou@gmail.com>
2025-09-05 21:41:17 +0800

    feat: posthog

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import { NuqsAdapter } from "nuqs/adapters/react"
 import { appIsDevEnv } from "./constant/system.ts"
 import nprogress from "nprogress"
 import "nprogress/nprogress.css"
+import posthog from "posthog-js"
+import { PostHogProvider } from "posthog-js/react"
+import { StrictMode } from "react"
 
 scan({
     enabled: appIsDevEnv,
@@ -18,8 +21,18 @@ nprogress.configure({
     speed: 500,
 })
 
+posthog.init("phc_GZSX0mz8f9kOkMec9GP8xckFOBARFwedhy7b1PRLv3Y", {
+    api_host: "https://us.i.posthog.com",
+    defaults: "2025-05-24",
+})
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <NuqsAdapter>
-        <App />
-    </NuqsAdapter>,
+    <StrictMode>
+        <PostHogProvider client={posthog}>
+            <NuqsAdapter>
+                <App />
+            </NuqsAdapter>
+            ,
+        </PostHogProvider>
+    </StrictMode>,
 )
