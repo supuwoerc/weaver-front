45021a2b8fe4e89f917a21477e4808d27d887be6
supuwoerc <zhangzhouou@gmail.com>
2025-10-08 15:04:08 +0800

    fix: default theme mode

--- src/lib/v-chart/theme.ts
+++ src/lib/v-chart/theme.ts
@@ -1,10 +1,13 @@
 import { themeAttribute } from "@/constant/system"
+import { system } from "@/store"
 import { initVChartArcoTheme } from "@visactor/vchart-arco-theme"
 import { observeAttribute } from "@visactor/vchart-theme-utils"
 
 const initChartThemeHelper = () => {
+    const { theme } = system.useSystemConfigStore.getState()
     const charThemeHelper = initVChartArcoTheme({
         isWatchingMode: false,
+        defaultMode: theme,
     })
     observeAttribute(document.body, themeAttribute, () => {
         charThemeHelper.switchVChartTheme(true)
